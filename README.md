a0e71e38c0b0846d51c8424e49e4407dcd20ab8a
ITeCK62 <yoplayzgaming@gmail.com>
2023-11-15 21:49:04 +0100

    V2

---
+++ README.md
@@ -0,0 +1,2 @@
+# MotdepasseGen
+Ce code génére un mot de passe aléatoire et le stock dans un fichier.txt.
--- motdepasse.c
+++ motdepasse.c
@@ -5,10 +5,11 @@
 #include <string.h>
 
 char key[100];
-char key2[100];
+char site2[100];
 char password[100];
 char name[100];
-char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+char site[100];
+char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#!";
 int i, length;
 
 // Fonction pour générer un mot de passe aléatoire
@@ -23,8 +24,8 @@ void randPass(){
     }
 }
 
-// Fonction pour enregistrer les informations de connexion dans un fichier
-void enregistrerFile(){
+// Fonction pour supprimer les mdp actuels
+void suppFile(){
     // Crée un pointeur de fichier pour stocker le nom d'utilisateur et le mot de passe
     FILE *fp;
     // Ouvre le fichier "password.txt" en mode écriture ("w")
@@ -34,22 +35,48 @@ void enregistrerFile(){
         printf("Erreur lors de l'ouverture du fichier.\n");
         return;
     }
-    // Écrit le nom d'utilisateur et le mot de passe dans le fichier
-    fprintf(fp, "%s : ", name);
+    // Supprime le contenu du fichier
+    fprintf(fp, "");
+    // Ferme le fichier
+    fclose(fp);
+}
+
+// Fonction pour enregistrer les informations de connexion dans un fichier
+void enregistrerFile(){
+    // Crée un pointeur de fichier pour stocker le nom d'utilisateur et le mot de passe
+    FILE *fp;
+    // Ouvre le fichier "password.txt" en mode ajout ("a")
+    fp = fopen("password.txt", "a");
+    // Vérifie si le fichier a été ouvert avec succès
+    if (fp == NULL) {
+        printf("Erreur lors de l'ouverture du fichier.\n");
+        return;
+    }
+    // Écrit le site, le nom d'utilisateur et le mot de passe dans le fichier
+    fprintf(fp, "%s | %s : ", site,name);
     fprintf(fp, "%s\n", password);
     // Ferme le fichier
     fclose(fp);
 }
 
-// Fonction pour rechercher une clé dans un tableau de clés
+// Fonction pour rechercher un mdp pour l'utilisateur
 void recherche(){
-    // Demande à l'utilisateur de saisir la clé à rechercher
-    printf("Enter the key: ");
-    scanf("%s", key2);
+    int test;
+    // Demande à l'utilisateur de saisir le site
+    printf("Enter site name: ");
+    scanf("%s", site2);
+    // Crée un pointeur de fichier pour stocker le nom d'utilisateur et le mot de passe
+    FILE *fp;
+    // Ouvre le fichier "password.txt" en mode ajout ("r")
+    fp = fopen("password.txt", "r");
+    while(!feof(fp)){
+        test = fgetc(fp);
+        printf("%c", test);
+    }
+    fclose(fp);
 }
 
-int main() {
-
+void ajoutMdp(){
     // Demande à l'utilisateur de saisir la clé à rechercher
     printf("Enter the key: ");
     scanf("%s", key);
@@ -65,12 +92,51 @@ int main() {
     printf("Enter the name of the user: ");
     scanf("%s", name);
 
+    // Demande à l'utilisateur de saisir le site
+    printf("Enter site name: ");
+    scanf("%s", site);
+
     // Enregistre les informations dans un fichier
     enregistrerFile();
 
     // Affiche le mot de passe et le nom de l'utilisateur
     printf("The password is: %s\n", password);
     printf("The name is: %s\n", name);
+}
 
+// Fonction qui demande à l'utilisateur la fonction à éxécuter
+void menu(){
+    // Init du choix
+    unsigned int choix = 0;
+    // Affiche pour l'utilisateur les fonctions
+    printf("- Ajouter un mdp ? '1'\n- Supprimer les mdp ? '2'\n- Quitter '3'\n");
+    // Récupére le choix de l'utilisateur
+    scanf("%u", &choix);
+    // Vérifie le choix de l'utilisateur
+    switch(choix){
+        // Si choix == 1
+        case 1:
+            // Lance la fonction d'ajout de mdp
+            ajoutMdp();
+            // Quitte le switch
+            break;
+        // Si choix == 2
+        case 2:
+            // Lance la fonction de suppression de tous les mdp
+            suppFile();
+            // Quitte le switch
+            break;
+        // Peut importe le choix autre qu'au dessus
+        default :
+            // Quitte le switch
+            break;
+    }
+}
+
+// Fonction main
+int main() {
+    // Lance la fonction du menu
+    menu();
+    //recherche();
     return 0;
 }
